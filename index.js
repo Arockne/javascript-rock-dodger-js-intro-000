1aceba823452082a961cf08cfc8b95b01fd91c8c
Daniel Calvin <arockne2003@yahoo.com>
2021-05-24 23:50:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -164,6 +164,7 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
   const code = e.which;
+  
   if ([LEFT_ARROW, RIGHT_ARROW].indexOf(code) > -1) {
     e.preventDefault();
     e.stopPropagation();
